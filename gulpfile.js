7edc45c9260a37e5193c2fc1eca7b33bbac027ae
Daria <nabatova.darya@protonmail.com>
2020-04-12 14:15:34 +0300

    index gulp

--- gulpfile.js
+++ gulpfile.js
@@ -27,6 +27,7 @@ gulp.task('scss', function () {
 gulp.task('css', function () {
     return gulp.src([
         'node_modules/normalize.css/normalize.css',
+        'node_modules/animate.css/animate.css'
         // 'node_modules/slick-carousel/slick/slick.css'
     ])
         .pipe(concat('libs.scss'))
@@ -46,7 +47,7 @@ gulp.task('js', function () {
 
 gulp.task('libs', function (){
     return gulp.src([
-        'node_modules/slick-carousel/slick/slick.js'
+        'node_modules/wow.js/dist/wow.js'
     ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
@@ -144,6 +145,6 @@ gulp.task('webp', function () {
 
 
 
-gulp.task('build', gulp.series('clean', 'images', 'script', 'svg', 'sprite', 'webp', 'export'));
+gulp.task('build', gulp.series('clean', 'images', 'script', 'svg', 'export'));
 
 gulp.task('default', gulp.parallel('css', 'scss', 'libs', 'js', 'browser-sync', 'watch'));
